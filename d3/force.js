a96668464e9c1b14ced262ce7a6d514893acd39a
Sergey Zubkov <ksevelyar@protonmail.com>
2022-11-06 18:55:52 +0300

    initial counters

--- d3/force.js
+++ d3/force.js
@@ -3,12 +3,14 @@ const graph = {
     { source: 0, target: 1 },
     { source: 1, target: 2 },
     { source: 2, target: 3 },
+    { source: 3, target: 4 },
   ],
   nodes: [
-    { name: 'a', id: 0, counter: 42 },
-    { name: 'b', id: 1 },
-    { name: 'c', id: 2 },
-    { name: 'd', id: 3, counter: 7 },
+    { name: 'dev1', id: 0, user_counter: '42' },
+    { name: 'dev2', id: 1 },
+    { name: 'dev3', id: 2 },
+    { name: 'dev4', id: 3, user_counter: '70' },
+    { name: 'dev5', id: 4, user_counter: '99' },
   ]
 }
 
@@ -22,24 +24,50 @@ const simulation =
     .force('charge', d3.forceManyBody())
     .force('center', d3.forceCenter(width / 2, height / 2))
 
-function ticked() {
-  d3.select('svg')
-    .selectAll('circle')
-    .data(graph.nodes)
-    .join('circle')
-    .attr('r', 15)
-    .attr('cx', data => data.x)
-    .attr('cy', data => data.y)
-    .attr('fill', '#6657A6')
+const svg = d3.select('svg')
+const node = svg.append('g')
+  .attr('fill', '#fff')
+  .selectAll('g')
+  .data(graph.nodes)
+  .join('g')
 
-  d3.select('svg')
+node
+  .append('circle')
+  .attr('r', 20)
+  .attr('fill', '#666')
+
+node.append('text')
+  .text(d => d.name)
+  .attr('fill', '#6657A6')
+  .attr('dy', 40)
+
+node
+  .filter(d => d.user_counter)
+  .append('circle')
+  .attr('r', data => String(data.user_counter).length * 6)
+  .attr('fill', '#B7B3A1')
+  .attr('cy', -10)
+  .attr('cx', 10)
+
+node.append('text')
+  .filter(d => d.user_counter)
+  .text(d => d.user_counter)
+  .attr('fill', 'black')
+  .attr('x', -1)
+  .attr('y', -5)
+
+function ticked() {
+  node
+    .attr('transform', data => 'translate(' + data.x + ', ' + data.y + ')')
+
+  svg
     .selectAll('line')
     .data(graph.links)
     .join('line').attr('stroke-width', 2)
 
     .attr('x1', node => node.source.x + 0.1).attr('y1', node => node.source.y + 0.1)
     .attr('x2', node => node.target.x).attr('y2', node => node.target.y)
-    .attr('stroke', '#6657A6')
+    .attr('stroke', '#666')
 }
 
 simulation.nodes(graph.nodes).on('tick', ticked)
